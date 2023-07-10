f5fb688b6e1ce4aafb30ed6539abc8c7fcfb9a6f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-10 13:11:51 +0200

    added example of querying

---
+++ 7_gpt_querying.py
@@ -0,0 +1,20 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+# Load pretrained model and tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+#model = GPT2LMHeadModel.from_pretrained("./gpt2_finetuned")
+
+# Encode input context
+while True:
+    input_context = input("\n\n\nInsert a query -> ")
+    input_ids = tokenizer.encode(input_context, return_tensors='pt')
+
+    # Generate text
+    output = model.generate(input_ids, max_length=100, temperature=0.7, num_return_sequences=1)
+
+    # Decode the output
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True).split(input_context)[-1]
+
+    print(generated_text)
+
