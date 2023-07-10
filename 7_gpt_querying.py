6cc1530c59cf586965adaf381ac00d52a673d9c0
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-10 13:22:17 +0200

    fiX

--- 7_gpt_querying.py
+++ 7_gpt_querying.py
@@ -11,7 +11,7 @@ while True:
     input_ids = tokenizer.encode(input_context, return_tensors='pt')
 
     # Generate text
-    output = model.generate(input_ids, max_length=100, temperature=0.7, num_return_sequences=1)
+    output = model.generate(input_ids, max_length=300, temperature=0.7, num_return_sequences=1)
 
     # Decode the output
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True).split(input_context)[-1]
