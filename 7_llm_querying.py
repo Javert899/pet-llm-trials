f23d35acc246c8b25b01a28bcbbb611b5de3cef8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-10 14:03:18 +0200

    trial

--- 7_llm_querying.py
+++ 7_llm_querying.py
@@ -2,10 +2,10 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Load pretrained model and tokenizer
-#tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-#model = GPT2LMHeadModel.from_pretrained("gpt2")
-tokenizer = T5Tokenizer.from_pretrained('t5-base')
-model = T5ForConditionalGeneration.from_pretrained('t5-base')
+tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
+model = GPT2LMHeadModel.from_pretrained("distilgpt2")
+#tokenizer = T5Tokenizer.from_pretrained('t5-base')
+#model = T5ForConditionalGeneration.from_pretrained('t5-base')
 
 # Encode input context
 while True:
@@ -13,7 +13,7 @@ while True:
     input_ids = tokenizer.encode(input_context, return_tensors='pt')
 
     # Generate the output
-    output = model.generate(input_ids)
+    output = model.generate(input_ids, max_length=200)
 
     # Decode the output
     for text in output:
