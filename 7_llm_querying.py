51ef06adf1080b3f014358f7b904ab506a5ff472
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-10 14:20:10 +0200

    revision

--- 7_llm_querying.py
+++ 7_llm_querying.py
@@ -6,15 +6,18 @@ tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
 model = GPT2LMHeadModel.from_pretrained("distilgpt2")
 #tokenizer = T5Tokenizer.from_pretrained('t5-base')
 #model = T5ForConditionalGeneration.from_pretrained('t5-base')
+#tokenizer = T5Tokenizer.from_pretrained('t5-large')
+#model = T5ForConditionalGeneration.from_pretrained('t5-large')
 
 # Encode input context
 while True:
     input_context = input("\n\n\nInsert a query -> ")
+    print(input_context)
     input_ids = tokenizer.encode(input_context, return_tensors='pt')
 
     # Generate the output
     output = model.generate(input_ids, max_length=200)
 
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True).strip()
 
     print(generated_text)
