15c9f1ddeffe101ee7db14f75b58d3d3038998ea
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-17 11:12:17 +0200

    fix

--- 10_train_llm_from_scratch.py
+++ 10_train_llm_from_scratch.py
@@ -25,7 +25,7 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 # Create a new model with a custom number of tokens
 # "max_new_tokens" is the new maximum number of tokens, you might need to adjust this number
 max_new_tokens = 100000
-config = GPT2LMHeadModel(config=GPT2Config(vocab_size=tokenizer.vocab_size + max_new_tokens))
+config = GPT2Config(vocab_size=tokenizer.vocab_size + max_new_tokens)
 model = GPT2LMHeadModel(config)
 
 # Define the training arguments
